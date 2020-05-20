a9335440203387b395d2859969a0d37ce477769b
Paolo Mason <mason.paolo@gmail.com>
2020-05-20 21:20:26 +0200

    ⚙ FIX: sir model, aggiunta libreria per calcolo equazioni differenziali di ordine 4

--- assets/js/app.js
+++ assets/js/app.js
@@ -1113,129 +1113,129 @@ async function getChart() {
 
 
 
-  let chart = null;
-  let skipEvery = 2;
+  // let chart = null;
+  // let skipEvery = 2;
 
-  let R0 = (Number(document.getElementById("beta").value) / Number(document.getElementById("alpha").value)).toFixed(2);
-  computeAndGraph(R0);
+  // let R0 = (Number(document.getElementById("beta").value) / Number(document.getElementById("alpha").value)).toFixed(2);
+  // computeAndGraph(R0);
 
-  function computeAndGraph(R0) {
-    let alpha = Number(document.getElementById("alpha").value);
-    let beta = Number(document.getElementById("beta").value);
-    let I0 = Number(document.getElementById("I0").value);
-    let N = Number(document.getElementById("N").value);
-    let delT = Number(document.getElementById("delT").value);
-    let maxTime = Number(document.getElementById("maxTime").value);
+  // function computeAndGraph(R0) {
+  //   let alpha = Number(document.getElementById("alpha").value);
+  //   let beta = Number(document.getElementById("beta").value);
+  //   let I0 = Number(document.getElementById("I0").value);
+  //   let N = Number(document.getElementById("N").value);
+  //   let delT = Number(document.getElementById("delT").value);
+  //   let maxTime = Number(document.getElementById("maxTime").value);
 
-    if (delT == 0) return;
+  //   if (delT == 0) return;
 
-    let I_ar = [];
-    let S_ar = [];
-    let R_ar = [];
-    let t_ar = [];
+  //   let I_ar = [];
+  //   let S_ar = [];
+  //   let R_ar = [];
+  //   let t_ar = [];
 
-    let I = I0;
-    let S = N;
-    let R = 0;
+  //   let I = I0;
+  //   let S = N;
+  //   let R = 0;
 
-    let counter = 0;
-    for (let t = 0; t <= maxTime; t += delT) {
-      let delS = -beta * S * I / N * delT;
-      let delI = beta * S * I / N * delT - alpha * I * delT;
-      let delR = alpha * I * delT;
-      S += delS;
-      I += delI;
-      R += delR;
-      if (counter % skipEvery == 0) {
-        I_ar.push(I);
-        S_ar.push(S);
-        R_ar.push(R);
-        t_ar.push(Number(t.toFixed(2)));
-      }
-      counter++;
-    }
+  //   let counter = 0;
+  //   for (let t = 0; t <= maxTime; t += delT) {
+  //     let delS = -beta * S * I / N * delT;
+  //     let delI = beta * S * I / N * delT - alpha * I * delT;
+  //     let delR = alpha * I * delT;
+  //     S += delS;
+  //     I += delI;
+  //     R += delR;
+  //     if (counter % skipEvery == 0) {
+  //       I_ar.push(I);
+  //       S_ar.push(S);
+  //       R_ar.push(R);
+  //       t_ar.push(Number(t.toFixed(2)));
+  //     }
+  //     counter++;
+  //   }
 
-    drawChart({
-      t: t_ar,
-      I: I_ar,
-      S: S_ar,
-      R: R_ar
-    }, N, R0);
+  //   drawChart({
+  //     t: t_ar,
+  //     I: I_ar,
+  //     S: S_ar,
+  //     R: R_ar
+  //   }, N, R0);
 
-  }
+  // }
 
 
-  function drawChart(output, y0, R0) {
-    var ctx = document.getElementById('chart').getContext('2d');
-    if (chart != undefined) {
-      chart.destroy();
-    }
-    chart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: output.t,
-        datasets: [{
-            label: 'Infected',
-            data: output.I,
-            borderColor: ['rgba(255, 99, 132, 1)'],
-            borderWidth: 1,
-            fill: false,
-            pointRadius: 1
-          },
-          {
-            label: 'Susceptible',
-            data: output.S,
-            borderColor: ['rgba(99, 255, 132, 1)'],
-            borderWidth: 1,
-            fill: false,
-            pointRadius: 1
-          },
-          {
-            label: 'Recovered',
-            data: output.R,
-            borderColor: ['rgba(99, 132, 255, 1)'],
-            borderWidth: 1,
-            fill: false,
-            pointRadius: 1
-          }
-        ]
-      },
-      options: {
-        animation: {
-          duration: 0
-        },
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true,
-              max: y0
-            },
-            scaleLabel: {
-              display: true,
-              labelString: 'Population'
-            }
-          }],
-          xAxes: [{
-            ticks: {
-              autoSkip: true,
-              maxTicksLimit: 30
-            },
-            scaleLabel: {
-              display: true,
-              labelString: 'Time'
-            }
+  // function drawChart(output, y0, R0) {
+  //   var ctx = document.getElementById('chart').getContext('2d');
+  //   if (chart != undefined) {
+  //     chart.destroy();
+  //   }
+  //   chart = new Chart(ctx, {
+  //     type: 'line',
+  //     data: {
+  //       labels: output.t,
+  //       datasets: [{
+  //           label: 'Infected',
+  //           data: output.I,
+  //           borderColor: ['rgba(255, 99, 132, 1)'],
+  //           borderWidth: 1,
+  //           fill: false,
+  //           pointRadius: 1
+  //         },
+  //         {
+  //           label: 'Susceptible',
+  //           data: output.S,
+  //           borderColor: ['rgba(99, 255, 132, 1)'],
+  //           borderWidth: 1,
+  //           fill: false,
+  //           pointRadius: 1
+  //         },
+  //         {
+  //           label: 'Recovered',
+  //           data: output.R,
+  //           borderColor: ['rgba(99, 132, 255, 1)'],
+  //           borderWidth: 1,
+  //           fill: false,
+  //           pointRadius: 1
+  //         }
+  //       ]
+  //     },
+  //     options: {
+  //       animation: {
+  //         duration: 0
+  //       },
+  //       scales: {
+  //         yAxes: [{
+  //           ticks: {
+  //             beginAtZero: true,
+  //             max: y0
+  //           },
+  //           scaleLabel: {
+  //             display: true,
+  //             labelString: 'Population'
+  //           }
+  //         }],
+  //         xAxes: [{
+  //           ticks: {
+  //             autoSkip: true,
+  //             maxTicksLimit: 30
+  //           },
+  //           scaleLabel: {
+  //             display: true,
+  //             labelString: 'Time'
+  //           }
 
-          }]
-        },
-        title: {
-          display: true,
-          text: "Epidemic over time | R0 = " + R0
-        },
-        maintainAspectRatio: false,
-        responsive: false
-      }
-    });
-  }
+  //         }]
+  //       },
+  //       title: {
+  //         display: true,
+  //         text: "Epidemic over time | R0 = " + R0
+  //       },
+  //       maintainAspectRatio: false,
+  //       responsive: false
+  //     }
+  //   });
+  // }
 
 
 
